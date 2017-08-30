06701e6fc2eb9001a700b7ff7349777acc62c578
ilgizkasymov <ilgizkasymov@gmail.com>
2017-08-30 16:27:34 +0600

    change migration

--- migrations/20170830061149-create-event.js
+++ migrations/20170830061149-create-event.js
@@ -9,7 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(126),
         allowNull: false,
         unique: true
       },
